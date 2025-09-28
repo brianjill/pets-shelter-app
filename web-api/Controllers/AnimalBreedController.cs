ff997e8e1bc792e4907a5683e6788c717b473e27
brianjill <brian.sarandi@soprasteria.com>
2025-09-28 10:35:32 +0200

    initial commit

---
+++ web-api/Controllers/AnimalBreedController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsShelterApi.Controllers;
+
+[Route("api/[controller]")]
+public class AnimalBreedController : Controller
+{
+    [HttpGet]
+    public IReadOnlyCollection<string>? GetBreed()
+    {
+        return new List<string> { "Cat", "Dog" };
+        
+    }
+}
